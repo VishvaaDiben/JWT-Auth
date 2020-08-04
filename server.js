6e3cd46ea089b5614905fd9346375a6cddcb555b
unknown <dibenvishvaa@gmail.com>
2020-08-04 18:02:52 +0800

    JWT Auth Tutorial https://youtu.be/mbsmsi7l3r4

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+const posts = [{ username: "Diben", title: "Post by Diben" }, {username: "Vishvaa", title: "Post by Vishvaa"}];
+
+app.get("/posts",authenticateToken, (req, res) => {
+    // req.user
+  res.json(posts.filter(post => post.username === req.user.name));
+})
+
+//Middleware
+function authenticateToken(req, res, next){
+ //verify user and get request
+ const authHeader = req.headers['authorization']
+ const token = authHeader && authHeader.split(' ')[1] 
+ if (token == null) return res.sendStatus(401)
+
+ jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+     if (err) return res.sendStatus(403)
+     req.user = user
+     next()
+ })
+}
+
+app.listen(3000);
